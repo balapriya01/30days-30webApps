67792b8f70a91a5da119a103d944d82f442e6792
balapriya01 <160397824+balapriya01@users.noreply.github.com>
2024-05-11 22:49:35 +0530

    Day 4

---
+++ API-fetch/src/App.js
@@ -0,0 +1,73 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import { v4 as uuidv4 } from "uuid";
+import Card from "./components/Card1";
+import Card2 from "./components/Card2";
+import Button from "./components/Button";
+
+const App = () => {
+  const [products, setProducts] = useState([]);
+  const [tech, setTech] = useState([]);
+  // let uuid = crypto.randomUUID();  //Was trying to add random unique ID, but failed drastically, finallly used uuidv4
+  const [click, setClick] = useState();
+
+  const handleClick = async () => {
+    let promise = await fetch("https://www.boredapi.com/api/activity");
+    let data = await promise.json();
+    setProducts([data]);
+    setClick(true);
+    // .then((response) => response.json())
+    // .then((data) => {
+    //   setProducts([data]);
+    //   // console.log(data);
+    // });
+  };
+
+  const handleTech = () => {
+    fetch("https://techy-api.vercel.app/api/json")
+      .then((response) => response.json())
+      .then((data) => {
+        setTech([data]);
+        setClick(false);
+        console.log(data);
+      });
+  };
+
+  useEffect(() => {
+    handleClick();
+    handleTech();
+  }, []);
+
+  return (
+    <>
+      <div className="container">
+        <div className="sub-con">
+          <div className="btn-con">
+            <Button name="Time Pass??" Click={handleClick} />
+            <Button name="Techyyy??" Click={handleTech} />
+          </div>
+          <div>
+            {click &&
+              products.map((product) => (
+                <Card
+                  key={product.key}
+                  product={product}
+                  activity={product.activity}
+                  type={product.type}
+                />
+              ))}
+            {click ||
+              tech.map((techreply) => (
+                <Card2
+                  key={uuidv4()} //Because, this API key was not containing any unique ID.
+                  message={techreply.message}
+                />
+              ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
